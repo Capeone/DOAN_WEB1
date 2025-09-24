1e3d9fc36f4b622f3dfade5bf990c0c8d7049271
phuongtheduy <phuongtheduy809@gmail.com>
2025-09-24 15:33:20 +0700

    all ok

---
+++ DOAN_WEB12/server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = { 
+  DB_SERVER: 'nangmanh.awdif.mongodb.net',
+  DB_USER: 'nnmanh203',
+  DB_PASS: 'nangmanh2003',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'nnmanh203@gmail.com', // Microsoft mail service
+  EMAIL_PASS: 'urmsdzpjujfltcrk',
+  JWT_SECRET: '<jwt_secret>',
+  JWT_EXPIRES: 1000000000000000000009999997770, // in milliseconds
+};
+
+module.exports = MyConstants;
